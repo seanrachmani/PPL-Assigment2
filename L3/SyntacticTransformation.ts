cc7e98395cd1f03c569d2b75713a15d584dec024
Sean Rachmani <sicsicrr11@gmail.com>
2026-05-20 18:59:32 +0300

    2.c part 1

--- L3/SyntacticTransformation.ts
+++ L3/SyntacticTransformation.ts
@@ -1,4 +1,4 @@
-import { ClassExp, ProcExp, Exp, Program } from "./L3-ast";
+import { ClassExp, ProcExp, Exp, Program, CExp, Binding, makeProcExp, makeIfExp, makeAppExp, makePrimOp, makeVarDecl, makeVarRef, makeLitExp} from "./L3-ast";
 import { Result, makeFailure } from "../shared/result";
 
 /*
@@ -7,8 +7,18 @@ Signature: class2proc(classExp)
 Type: ClassExp => ProcExp
 */
 export const class2proc = (exp: ClassExp): ProcExp =>
-    //@TODO
-    exp;
+    makeProcExp(exp.fields, [makeProcExp([makeVarDecl("msg")], [methods2if(exp.methods)])]);
+
+//helper function for 10000 ifs(so FUN ==))) )
+//error if we didnt find the method
+export const methods2if = (methods: Binding[]) : CExp =>
+    (methods.length === 0) ? makeLitExp({tag: "SymbolSExp", val: "error"}) :
+    makeIfExp(
+        //test:   //makeappexp
+        makeAppExp(makePrimOp("eq?"), [makeVarRef("msg"), makeLitExp({tag: "SymbolSExp", val: methods[0].var.var})]),
+        //then, else:
+        methods[0].val, methods2if(methods.slice(1)));
+
 
 
 /*
@@ -20,3 +30,4 @@ Type: [Exp | Program] => Result<Exp | Program>
 export const transform = (exp: Exp | Program): Result<Exp | Program> =>
     //@TODO
     makeFailure("ToDo");
+
